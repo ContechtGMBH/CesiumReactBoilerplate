90b9e4f2b4b83020bda11727b993a3dfabf1152b
dmh126 <dharasymczuk@gmail.com>
2017-05-05 12:12:41 +0200

    redux + d3 + others

--- src/cesium/CesiumGlobe.jsx
+++ src/cesium/CesiumGlobe.jsx
@@ -1,91 +1,40 @@
 import React, {Component} from "react";
+import {connect} from 'react-redux';
 
 import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
-import BingMapsImageryProvider from "cesium/Source/Scene/BingMapsImageryProvider";
-import CesiumTerrainProvider from "cesium/Source/Core/CesiumTerrainProvider";
-import GeoJsonDataSource from "cesium/Source/DataSources/GeoJsonDataSource";
 import Cesium from "cesium/Source/Cesium";
-import Color from "cesium/Source/Core/Color";
 
+import TopBar from "./TopBar"
+import CesiumBillboard from "./CesiumBillboard";
+import CesiumDatasources from "./CesiumDatasources";
 
-const BING_MAPS_URL = "//dev.virtualearth.net";
-const BING_MAPS_KEY = "ApDPY15x9lCXO5Hw89M1G5Q84_BlKalPbjor8GvKGj2UAnVtzlT5UT-zrylU1e48";
-const STK_TERRAIN_URL = "//assets.agi.com/stk-terrain/world";
-
-import layerTracks from "../assets/tracks.geojson";
-import layerSignals from "../assets/signals.geojson";
-import layerPlatforms from "../assets/platforms.geojson";
-
-import layerCounties from "../assets/counties.geojson";
-
-
-
-export default class CesiumGlobe extends Component {
+class CesiumGlobe extends Component {
     constructor (props){
       super(props);
 
       this.state = {
           viewerLoaded : false,
       }
-
-      this.zoomToDublin = this.zoomToDublin.bind(this);
-      this.zoomToCork = this.zoomToCork.bind(this);
-      this.zoomToGalway = this.zoomToGalway.bind(this);
-      this.population = this.population.bind(this);
-      this.resetRails = this.resetRails.bind(this);
-
-
     }
 
 
     componentDidMount() {
-        const imageryProvider = new BingMapsImageryProvider({
-            url : BING_MAPS_URL,
-            key : BING_MAPS_KEY,
-        });
-
-        const terrainProvider = new CesiumTerrainProvider({
-            url : STK_TERRAIN_URL
-        });
-
-        this.tracks = new GeoJsonDataSource.load(layerTracks, {
-
-        });
-        this.signals = new GeoJsonDataSource.load(layerSignals, {
-              strokeWidth: 1,
-              markerSymbol: 'rail',
-              markerColor: Color.BLUE,
-
-        });
-        this.platforms = new GeoJsonDataSource.load(layerPlatforms, {
-            stroke: Color.RED,
-            strokeWidth: 10,
-
-        });
-
-        this.counties = new GeoJsonDataSource.load(layerCounties, {
-
-        });
 
         this.viewer = new Viewer(this.cesiumContainer, {
-            //animation : false,
+            animation : false,
             //baseLayerPicker : false,
             //fullscreenButton : false,
             //geocoder : false,
-            //homeButton : false,
+            homeButton : false,
             //infoBox : false,
-            //sceneModePicker : false,
+            sceneModePicker : false,
             //selectionIndicator : true,
-            //timeline : false,
-            //navigationHelpButton : false,
+            timeline : false,
+            navigationHelpButton : false,
             //scene3DOnly : true,
             //imageryProvider,
             //terrainProvider,
         });
-
-        this.viewer.dataSources.add(this.tracks);
-        this.viewer.dataSources.add(this.signals);
-        this.viewer.dataSources.add(this.platforms);
 
         this.viewer.camera.flyTo({destination : Cesium.Cartesian3.fromDegrees(-8.484, 54.272, 1500.0)})
 
@@ -97,55 +46,6 @@ export default class CesiumGlobe extends Component {
         if(this.viewer) {
             this.viewer.destroy();
         }
-    }
-
-    zoomToDublin(){
-        this.viewer.camera.flyTo({destination : Cesium.Cartesian3.fromDegrees(-6.246, 53.353, 250.0)})
-    }
-
-    zoomToCork(){
-        this.viewer.camera.flyTo({destination : Cesium.Cartesian3.fromDegrees(-8.455, 51.902, 300.0)})
-    }
-
-    zoomToGalway(){
-        this.viewer.camera.flyTo({destination : Cesium.Cartesian3.fromDegrees(-9.045, 53.2725, 300.0)})
-    }
-
-    population(){
-        this.viewer.dataSources.removeAll()
-        this.viewer.dataSources.add(this.counties);
-
-        this.counties.then(res => {
-            var entities = res.entities.values;
-
-            var colorHash = {};
-            for (var i = 0; i < entities.length; i++) {
-                var entity = entities[i];
-                var name = entity.name;
-                var color = colorHash[name];
-                if (!color) {
-                    color = Cesium.Color.fromRandom({
-                        alpha : 1.0
-                    });
-                    colorHash[name] = color;
-                }
-                entity.polygon.material = color;
-                entity.polygon.outline = false;
-                entity.polygon.extrudedHeight = entity.properties.TOTAL2011 / 20.0;
-            }
-        });
-
-        this.viewer.zoomTo(this.counties)
-    }
-
-    resetRails(){
-        this.viewer.dataSources.removeAll()
-
-        this.viewer.dataSources.add(this.tracks);
-        this.viewer.dataSources.add(this.signals);
-        this.viewer.dataSources.add(this.platforms);
-
-        this.viewer.zoomTo(this.tracks)
     }
 
 
@@ -165,14 +65,17 @@ export default class CesiumGlobe extends Component {
 
     render() {
         const containerStyle = {
-            width: '100%',
-            height: '100%',
-            top: 0,
+            width: 'auto',
+            height: 'auto',
+            overflow: "auto",
+            top: 35,
             left: 0,
             bottom: 0,
             right: 0,
             position: 'fixed',
             display : "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
             alignItems : "stretch",
         };
 
@@ -181,9 +84,14 @@ export default class CesiumGlobe extends Component {
         }
 
         const contents = this.renderContents()
+        //console.log(this.props)
+
+        const billboard = (this.viewer) ? <CesiumBillboard scene={this.viewer.scene} /> : null;
+        const dataSources = (this.viewer) ? <CesiumDatasources dataSources={this.viewer.dataSources} /> : null;
 
         return (
             <div>
+            <TopBar />
                 <div className="cesiumGlobeWrapper" style={containerStyle}>
                     <div
                         className="cesiumWidget"
@@ -191,38 +99,19 @@ export default class CesiumGlobe extends Component {
                         style={widgetStyle}
                     >
                         {contents}
+                        {billboard}
+                        {dataSources}
                     </div>
                 </div>
-
-                <div className="panel-main">
-                    <div className="tabs">
-                        <a className="tab">tab1</a>
-                        <a className="tab">tab2</a>
-                        <a className="tab">tab3</a>
-                        <hr/>
-                    </div>
-                    <div className="tab-content">
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-
-                        <button className="btn" onClick={this.zoomToDublin}>Fly to Dublin</button>
-                        <br/>
-                        <br/>
-                        <button className="btn" onClick={this.zoomToCork}>Fly to Cork</button>
-                        <br/>
-                        <br/>
-                        <button className="btn" onClick={this.zoomToGalway}>Fly to Galway</button>
-                        <br/>
-                        <br/>
-                        <button className="btn" onClick={this.population}>3D statistics</button>
-                        <br/>
-                        <br/>
-                        <button className="btn" onClick={this.resetRails}>Reset</button>
-                    </div>
-                </div>
-        </div>
+            </div>
         );
     }
 }
+
+function mapStateToProps(state){
+  return {
+    testing: state.test
+  }
+}
+
+export default connect(mapStateToProps)(CesiumGlobe);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import allReducers from './reducers/index';
 import App from './App';
 import './index.css';
 
 import "cesium/Source/Widgets/widgets.css";
 
 import buildModuleUrl from "cesium/Source/Core/buildModuleUrl";
+
 buildModuleUrl.setBaseUrl('./cesium/');
 
+const store = createStore(allReducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
